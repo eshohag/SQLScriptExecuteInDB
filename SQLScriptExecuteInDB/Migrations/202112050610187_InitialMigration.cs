3a70c07d4a775d092c282a758719828a69cf5d33
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-12-05 12:12:59 +0600

    Table("Users", Schema = "UserRegistrations")

---
+++ SQLScriptExecuteInDB/Migrations/202112050610187_InitialMigration.cs
@@ -0,0 +1,27 @@
+﻿namespace SQLScriptExecuteInDB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "UserRegistrations.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        PhoneNo = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("UserRegistrations.Users");
+        }
+    }
+}
